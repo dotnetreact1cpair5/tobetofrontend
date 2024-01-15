fd10c246155e156f9045ee6ff43fb6b36304edce
Muhammet Ali Baran <77547838+Alibaran-0415@users.noreply.github.com>
2024-01-15 09:35:46 +0300

    Chartjs added, some changes

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,18 +1,26 @@
-// ProfileCard.tsx
-
 import React, { ReactNode } from 'react';
 import { Card } from 'flowbite-react';
 
 interface ProfileCardProps {
   title?: string;
   children?: ReactNode;
+  isShareImgVisible?: boolean;
 }
 
-const ProfileCard: React.FC<ProfileCardProps> = ({ title, children }) => {
+const ProfileCard: React.FC<ProfileCardProps> = ({ title, children, isShareImgVisible = true }) => {
   return (
-    <Card className="max-w-sm my-4">
-      <div>
-        <p className="section-title">{title}</p>
+    <Card className="row-span-2 col-span-2">
+      <div className="section-title-container">
+        <p className="section-title">
+          {title}
+          {isShareImgVisible && (
+            <img
+              src="/public/eye.svg"
+              alt="share"
+              className="ali"
+            />
+          )}
+        </p>
         <hr className="section-divider" />
       </div>
       <div>{children}</div>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import ReactCalendarHeatmap from 'react-calendar-heatmap';
-import 'react-calendar-heatmap/dist/styles.css';
-import { Card } from 'flowbite-react'; 
-import '../Profile.css';
-import ProfileCard from '../components/ProfileCard';
-import { fetchAccountData } from '../services/AccountService';
-import { fetchExperiences } from '../services/ExperienceService';
+import React, { useState, useEffect } from "react";
+import ReactCalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
+import { Card } from "flowbite-react";
+import "../Profile.css";
+import ProfileCard from "../components/ProfileCard";
+import { fetchAccountData } from "../services/AccountService";
+import { fetchExperiences } from "../services/ExperienceService";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
+import RadarChart from "../components/RadarChart";
+import sourceData from "../data/sourceData";
 
 interface ProfileData {
   id: number;
@@ -45,14 +49,46 @@ const Profile: React.FC = () => {
 
         setLoading(false);
       } catch (error) {
-        console.error('Veri alınırken hata oluştu:', error);
-        setError('Veri alınırken bir hata oluştu');
+        console.error("Veri alınırken hata oluştu:", error);
+        setError("Veri alınırken bir hata oluştu");
         setLoading(false);
       }
     };
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    const canvasElement = document.getElementById(
+      "radarChart"
+    ) as HTMLCanvasElement;
+
+    if (canvasElement) {
+      const existingChart = ChartJS.getChart(canvasElement);
+
+      if (existingChart) {
+        existingChart.destroy();
+      }
+
+      new ChartJS(canvasElement, {
+        type: "radar",
+        data: {
+          labels: sourceData.map((data) => data.label),
+          datasets: [
+            {
+              label: "Count",
+              data: sourceData.map((data) => data.value),
+              backgroundColor: [
+                "rgba(43,63,229,0.8)",
+                "rgba(250,192,19,0.8)",
+                "rgba(253,135,135,0.8)",
+              ],
+            },
+          ],
+        },
+      });
+    }
+  }, [sourceData]);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -63,140 +99,227 @@ const Profile: React.FC = () => {
   }
 
   return (
-    <div className="container1">
-      <div className='veli'>
+    <main
+      id="profile-page"
+      className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 profile-page-container"
+    >
+            <div className="top">
+              <span> 
+                <img
+                src="/public/edit2.svg"
+                alt="edit"
+                className="ali"
+                />
+              </span>
+
+              <span>
+                <img
+                  src="/public/share.svg"
+                  alt="share"
+                  className="ali"
+                  />
+              </span>
+            </div>
+      <div className="grid grid-rows-3 gap-4 md:col-span-1">
         {profileData.map((data) => (
-          <ProfileCard key={data.id}>
+          <ProfileCard key={data.id} isShareImgVisible={false}>
             <div className="cv-pp">
-              <img src="/public/images.png" alt="Name" className="cvName cvIcon" />
+              <img
+                src="/public/images.png"
+                alt="Name"
+                className="cvName cvIcon"
+              />
             </div>
             <div className="cv-info">
               <div className="info-box">
                 <div className="info-icon-name">
-                  <img src="/public/cv-name.svg" alt="Name" className="cvName" />
+                  <img
+                    src="/public/cv-name.svg"
+                    alt="Name"
+                    className="cvName"
+                  />
                 </div>
                 <div className="info-text">
-                  <span className="header">Ad Soyad</span><br/>
-                  <span className="text">{data.firstName} {data.lastName}</span>
+                  <span className="header">Ad Soyad</span>
+                  <br />
+                  <span className="text">
+                    {data.firstName} {data.lastName}
+                  </span>
                 </div>
               </div>
 
               <div className="info-box">
                 <div className="info-icon-name">
-                  <img src="/public/cv-date.svg" alt="Date" className="cvDate" />
+                  <img
+                    src="/public/cv-date.svg"
+                    alt="Date"
+                    className="cvDate"
+                  />
                 </div>
                 <div className="info-text">
-                  <span className="header">Doğum Tarihi</span><br/>
-                  <span className="text">{new Date(data.birthDate).toLocaleDateString()}</span>
+                  <span className="header">Doğum Tarihi</span>
+                  <br />
+                  <span className="text">
+                    {new Date(data.birthDate).toLocaleDateString()}
+                  </span>
                 </div>
               </div>
 
               <div className="info-box">
                 <div className="info-icon-name">
-                  <img src="/public/cv-mail.svg" alt="Mail" className="cvMail" />
+                  <img
+                    src="/public/cv-mail.svg"
+                    alt="Mail"
+                    className="cvMail"
+                  />
                 </div>
                 <div className="info-text">
-                  <span className="header">TC Kimlik No</span><br/>
+                  <span className="header">TC Kimlik No</span>
+                  <br />
                   <span className="text">{data.nationalId}</span>
                 </div>
               </div>
 
               <div className="info-box">
                 <div className="info-icon-name">
-                  <img src="/public/cv-phone.svg" alt="Mail" className="cvPhone" />
+                  <img
+                    src="/public/cv-phone.svg"
+                    alt="Mail"
+                    className="cvPhone"
+                  />
                 </div>
                 <div className="info-text">
-                  <span className="header">Telefon Numarası</span><br/>
+                  <span className="header">Telefon Numarası</span>
+                  <br />
                   <span className="text">{data.phoneNumber}</span>
                 </div>
               </div>
-            </div>      
+            </div>
           </ProfileCard>
         ))}
 
-        <ProfileCard title="Hakkımda">
-          Info
-        </ProfileCard>
+        <ProfileCard title="Hakkımda" isShareImgVisible={false}>Info</ProfileCard>
 
-        <ProfileCard title="Yetkinliklerim">
-          Info
-        </ProfileCard>
+        <ProfileCard title="Yetkinliklerim" isShareImgVisible={false}>Info</ProfileCard>
 
-        <ProfileCard title="Yabancı Dillerim">
-          Info
-        </ProfileCard>
+        <ProfileCard title="Yabancı Dillerim" isShareImgVisible={false}>Info</ProfileCard>
 
-        <ProfileCard title="Sertifikalarım">
-          Info
-        </ProfileCard>
+        <ProfileCard title="Sertifikalarım" isShareImgVisible={false}>Info</ProfileCard>
 
-        <ProfileCard title="Medya Hesaplarım">
-        <a className="cv-instagram" target="_blank" href="/https://www.instagram.com/">Tıkla</a>
+        <ProfileCard title="Medya Hesaplarım" isShareImgVisible={false}> 
+          <a
+            className="cv-instagram"
+            target="_blank"
+            href="/https://www.instagram.com/"
+          >
+            Tıkla
+          </a>
         </ProfileCard>
       </div>
 
       {/* RIGHT SIDE */}
-      <div className='ali'>
+      <div className="grid grid-rows-2 gap-4 md:col-span-2">
         <ProfileCard title="Tobeto İşte Başarı Modelim">
-        <div className="row">
-            <div className="leftSide">Chart</div>
-            <div className="rightSide">
+          <div className="grid grid-cols-2">
+            <div className="col-span-1">
+              <RadarChart />
+            </div>
+            <div className="col-span-1">
               <div className="radar-labels">
-                <div className="label"><span className="legend legend8">4.9</span><span className="legendName">Yeni dünyaya hazırlanıyorum</span></div>
-                <div className="label"><span className="legend legend8">4.9</span><span className="legendName">Yeni dünyaya hazırlanıyorum</span></div>
-                <div className="label"><span className="legend legend8">4.9</span><span className="legendName">Yeni dünyaya hazırlanıyorum</span></div>
-                <div className="label"><span className="legend legend8">4.9</span><span className="legendName">Yeni dünyaya hazırlanıyorum</span></div>
-                <div className="label"><span className="legend legend8">4.9</span><span className="legendName">Yeni dünyaya hazırlanıyorum</span></div>
-                <div className="label"><span className="legend legend8">4.9</span><span className="legendName">Yeni dünyaya hazırlanıyorum</span></div>
-                <div className="label"><span className="legend legend8">4.9</span><span className="legendName">Yeni dünyaya hazırlanıyorum</span></div>
-                <div className="label"><span className="legend legend8">4.9</span><span className="legendName">Yeni dünyaya hazırlanıyorum</span></div>
+                <div className="label">
+                  <span className="legend legend8">4.9</span>
+                  <span className="legendName">
+                    Yeni dünyaya hazırlanıyorum
+                  </span>
+                </div>
+                <div className="label">
+                  <span className="legend legend8">4.9</span>
+                  <span className="legendName">
+                    Yeni dünyaya hazırlanıyorum
+                  </span>
+                </div>
+                <div className="label">
+                  <span className="legend legend8">4.9</span>
+                  <span className="legendName">
+                    Yeni dünyaya hazırlanıyorum
+                  </span>
+                </div>
+                <div className="label">
+                  <span className="legend legend8">4.9</span>
+                  <span className="legendName">Yeni dünyaya hazırlanıyorum</span>
+                </div>
+                <div className="label">
+                  <span className="legend legend8">4.9</span>
+                  <span className="legendName">
+                    Yeni dünyaya hazırlanıyorum
+                  </span>
+                </div>
+                <div className="label">
+                  <span className="legend legend8">4.9</span>
+                  <span className="legendName">
+                    Yeni dünyaya hazırlanıyorum
+                  </span>
+                </div>
+                <div className="label">
+                  <span className="legend legend8">4.9</span>
+                  <span className="legendName">
+                    Yeni dünyaya hazırlanıyorum
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </ProfileCard>
 
-        <ProfileCard title="Tobeto Seviye Testlerim">
-            <div className="envantoryList"><div className=" envantory-result"><div className="d-flex justify-content-between w-100"><span className="examName">Herkes için Kodlama 1B Değerlendirme Sınavı<br/></span><span className="examTime">12-10-2023</span></div><span className="examResult">76.00</span></div>
+        <ProfileCard title="Tobeto Seviye Testlerim" isShareImgVisible={false}>
+          <div className="envantoryList">
+            <div className=" envantory-result">
+              <div className="d-flex justify-content-between w-100">
+                <span className="examName">
+                  Herkes için Kodlama 1B Değerlendirme Sınavı
+                  <br />
+                </span>
+                <span className="examTime">12-10-2023</span>
+              </div>
+              <span className="examResult">76.00</span>
             </div>
+          </div>
         </ProfileCard>
 
-        <ProfileCard title="Yetkinlik Rozetlerim">
-        <div className="badge-container">
-        {Array.from({ length: 3 }, (_, index) => (
-        <div key={index} className="badge-card">
-        </div>
-      ))}
-        </div>
+        <ProfileCard title="Yetkinlik Rozetlerim" isShareImgVisible={false}>
+          <div className="badge-container">
+            {Array.from({ length: 3 }, (_, index) => (
+              <div key={index} className="badge-card"></div>
+            ))}
+          </div>
         </ProfileCard>
 
-        <ProfileCard title="Aktivite Haritam">
+        <ProfileCard title="Aktivite Haritam" isShareImgVisible={false}>
           <ReactCalendarHeatmap
-            startDate={new Date('2022-01-01')}
-            endDate={new Date('2022-12-31')}
-            values={[
-              { date: '2022-01-01', count: 3 },
-            ]}
+            startDate={new Date("2022-01-01")}
+            endDate={new Date("2022-12-31")}
+            values={[{ date: "2022-01-01", count: 3 }]}
           />
         </ProfileCard>
 
         {experienceData.map((data) => (
-  <ProfileCard title="Eğitim Hayatım ve Deneyimlerim" key={data.id}>
-  <div className="cv-info">
-      <div className="info-box">
-        <span className="text">
-          {data.companyName}<br />
-          {data.cityId} <br />
-          {data.endDate} <br />
-          {data.startDate} <br />
-          {data.position}
-        </span>
+          <ProfileCard title="Eğitim Hayatım ve Deneyimlerim" key={data.id} isShareImgVisible={false}>
+            <div className="cv-info">
+              <div className="info-box">
+                <span className="text">
+                  {data.companyName}
+                  <br />
+                  {data.cityId} <br />
+                  {data.endDate} <br />
+                  {data.startDate} <br />
+                  {data.position}
+                </span>
+              </div>
+            </div>
+          </ProfileCard>
+        ))}
       </div>
-  </div>
-</ProfileCard>
-))}
-
-      </div>
-    </div>
+    </main>
   );
 };
 
